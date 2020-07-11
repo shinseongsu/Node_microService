a58e34ad100d861a3cbbc22c64cf82a903eb7db5
Shin seongsu <tlstjdtn321@naver.com>
2020-07-11 22:12:16 +0900

    url

--- ch2/monolithic.js
+++ ch2/monolithic.js
@@ -2,6 +2,11 @@ const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
 
+const members = require('./monolithic_members.js');
+const goods = require('./monolithic_goods.js');
+
+const purchases = require('./monolithic_purchases.js');
+
 var server = http.createServer((req, res) => {
     var method = req.method;
     var uri = url.parse(req.url, true);
@@ -31,5 +36,24 @@ var server = http.createServer((req, res) => {
 }).listen(8000);
 
 function onRequest(res, method, pathname, params) {
-    res.end("response!");
+    
+    switch(pathname) {
+        case "/members":
+            members.onRequest(res, method, pathname, response);
+            break;
+        case "/goods":
+            goods.onRequest(res, method, pathname, response);
+            break;
+        case "/purchases":
+            purchases.onRequest(res, method, pathname, params, response);
+            break;
+        default:
+            res.writeHead(404);
+            return res.end();
+    }
+}
+
+function response(res, packet) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(packet));
 }
